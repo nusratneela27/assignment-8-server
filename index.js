dad453302de4e0e1f041bf927092a7a9d59db6f3
neela <nusratneela27@gmail.com>
2024-05-19 22:49:53 +0600

    server add

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        console.log("connected to MongoDB");
+
+        const db = client.db('women-cloth');
+        const productsCollection = db.collection('products');
+
+        // Get All Products
+        app.get("/products", async (req, res) => {
+            const result = await productsCollection.find().toArray()
+            res.send(result)
+        })
+
+        //Get Single Details Products
+        app.get("/products/:id", async (req, res) => {
+            const id = (req.params.id)
+            const query = { _id: new ObjectId(id) }
+            const result = await productsCollection.findOne(query)
+            res.send(result);
+        });
+
+    } finally {
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
